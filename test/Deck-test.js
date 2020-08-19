7dc7720bc3d707835a1b5888a32580dc3f69f767
Stacy Potten <stacylynnaz@gmail.com>
2020-08-18 20:39:22 -0600

    Add tests for storing and counting cards in deck class

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -14,4 +14,15 @@ describe ('Deck', function() {
     const deck = new Deck();
     expect(deck).to.be.an.instanceof(Deck);
   });
+
+  it.skip('should store an array of cards', function() {
+    const deck = new Deck();
+    expect(deck.cards).to.equal([]);
+  });
+
+  it.skip('should know how many cards are in the deck', function() {
+    const deck = new Deck();
+    deck.countCards();
+    expect(deck.countCards()).to.equal(deck.cards.length);
+  });
 })
