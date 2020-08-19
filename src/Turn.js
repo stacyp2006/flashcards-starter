dca5200f2e181b55eb40336ae275c70bcfe4099f
Stacy Potten <stacylynnaz@gmail.com>
2020-08-18 19:32:38 -0600

    Create a Turn class with required methods

---
+++ src/Turn.js
@@ -0,0 +1,28 @@
+class Turn {
+  constructor(userGuess, card) {
+    this.userGuess = userGuess;
+    this.card = card;
+  }
+  returnGuess() {
+    return this.userGuess;
+  }
+  returnCard() {
+    return this.card;
+  }
+  evaluateGuess() {
+    if (this.userGuess === this.card.correctAnswer) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  giveFeedback() {
+    if (this.evaluateGuess() === true) {
+      return 'Correct!';
+    } else {
+      return 'Incorrect!';
+    }
+  }
+}
+
+module.exports = Turn;
