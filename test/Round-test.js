c8be32f17afe4e775d03e288fa6257b8eaee5f0b
Stacy Potten <stacylynnaz@gmail.com>
2020-08-20 16:41:29 -0600

    Refactor to add arrow functions to Round-test.js

--- test/Round-test.js
+++ test/Round-test.js
@@ -17,69 +17,69 @@ describe ('Round', function() {
     done();
   });
 
-  it('should be a function', function() {
+  it('should be a function', () => {
     expect(Round).to.be.a('function');
   });
 
-  it('should be an instance of Round', function() {
+  it('should be an instance of Round', () => {
     expect(round).to.be.an.instanceof(Round);
   });
 
-  it('should start round with a deck of cards', function() {
+  it('should start round with a deck of cards', () => {
     expect(round.deck).to.deep.equal([card1, card2, card3]);
   });
 
-  it('should start round with no turns', function() {
+  it('should start round with no turns', () => {
     expect(round.turns).to.equal(0);
   });
 
-  it('should start round with no correct guesses', function() {
+  it('should start round with no correct guesses', () => {
     expect(round.correctGuesses).to.equal(0);
   });
 
-  it('should start round with no incorrect guesses', function() {
+  it('should start round with no incorrect guesses', () => {
     expect(round.incorrectGuesses).to.deep.equal([]);
   });
 
-  it.skip('should count turns', function() {
+  it.skip('should count turns', () => {
     round.takeTurn(guess);
     expect(round.turns).to.equal(1);
   });
 
-  it('should instantiate a new Turn when a guess is made', function() {
+  it('should instantiate a new Turn when a guess is made', () => {
     round.takeTurn();
     expect(turn).to.be.an.instanceof(Turn);
   });
 
-  it.skip('should record a correct guess', function() {
+  it.skip('should record a correct guess', () => {
     round.takeTurn(guess);
     expect(round.correctGuesses).to.equal(1);
   });
 
-  it.skip('should record an incorrect guess', function() {
+  it.skip('should record an incorrect guess', () => {
     round.takeTurn(guess);
     expect(round.incorrectGuesses).to.deep.equal([card.id]);
   });
 
-  it.skip('should return the current card being played', function() {
+  it.skip('should return the current card being played', () => {
     round.returnCurrentCard();
 
     expect(round.returnCurrentCard()).to.equal(card1.detail);
   });
 
-  it.skip('should make the next card the current card', function() {
+  it.skip('should make the next card the current card', () => {
     round.takeTurn(guess);
     round.returnCurrentCard();
 
     expect(round.returnCurrentCard()).to.equal(card2.detail);
   });
 
-  it.skip('should calculate the percent correct', function() {
+  it.skip('should calculate the percent correct', () => {
     round.calculatePercentCorrect(5, 10);
     expect(round.calculatePercentCorrect()).to.equal(50);
   });
 
-  it.skip('should end the round', function() {
+  it.skip('should end the round', () => {
     round.endRound(50);
     expect(round.endRound).to.equal('** Round over! ** You answered 50% of the questions correctly!');
   });
