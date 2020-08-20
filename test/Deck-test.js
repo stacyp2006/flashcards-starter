c22de1568ef56527929860fe4e3e9e536475e274
Stacy Potten <stacylynnaz@gmail.com>
2020-08-20 16:19:46 -0600

    Refactor Deck-test.js to add beforeEach()

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -6,37 +6,30 @@ const Deck = require('../src/deck');
 
 
 describe ('Deck', function() {
+  let deck;
+  beforeEach((done) => {
+    card1 = new Card();
+    card2 = new Card();
+    card3 = new Card();
+    deck = new Deck([card1, card2, card3]);
+    done();
+  })
   it('should be a function', function() {
-    const card1 = new Card();
-    const card2 = new Card();
-    const card3 = new Card();
-    const deck = new Deck([card1, card2, card3]);
+
     expect(Deck).to.be.a('function');
   });
 
   it('should be an instance of Deck', function() {
-    const card1 = new Card();
-    const card2 = new Card();
-    const card3 = new Card();
-    const deck = new Deck([card1, card2, card3]);
+
     expect(deck).to.be.an.instanceof(Deck);
   });
 
   it('should store an array of cards', function() {
-    const card1 = new Card();
-    const card2 = new Card();
-    const card3 = new Card();
-
-    const deck = new Deck([card1, card2, card3]);
 
     expect(deck.cards).to.deep.equal([card1, card2, card3]);
   });
 
   it('should know how many cards are in the deck', function() {
-    const card1 = new Card();
-    const card2 = new Card();
-    const card3 = new Card();
-    const deck = new Deck([card1, card2, card3]);
 
     deck.countCards();
     expect(deck.countCards()).to.equal(deck.cards.length);
