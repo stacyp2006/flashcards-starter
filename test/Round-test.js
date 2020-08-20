ff98b8ebddf2356f8af68e8868ccdb828a6e4082
Stacy Potten <stacylynnaz@gmail.com>
2020-08-20 16:32:31 -0600

    Refactor to add beforeEach() to Round-test.js

--- test/Round-test.js
+++ test/Round-test.js
@@ -7,158 +7,80 @@ const Turn = require('../src/Turn');
 const Round = require('../src/Round');
 
 describe ('Round', function() {
-  it('should be a function', function() {
-    const card1 = new Card();
-    const card2 = new Card();
-    const card3 = new Card();
-    const deck = new Deck([card1, card2, card3]);
-    const round = new Round(deck);
+  let round;
+  beforeEach((done) => {
+    card1 = new Card();
+    card2 = new Card();
+    card3 = new Card();
+    deck = new Deck([card1, card2, card3]);
+    round = new Round(deck);
+    done();
+  });
 
+  it('should be a function', function() {
     expect(Round).to.be.a('function');
   });
 
   it('should be an instance of Round', function() {
-    const card1 = new Card();
-    const card2 = new Card();
-    const card3 = new Card();
-    const deck = new Deck([card1, card2, card3]);
-    const round = new Round(deck);
-
     expect(round).to.be.an.instanceof(Round);
   });
 
   it('should start round with a deck of cards', function() {
-    const card1 = new Card();
-    const card2 = new Card();
-    const card3 = new Card();
-    const deck = new Deck([card1, card2, card3]);
-    const round = new Round(deck);
-
     expect(round.deck).to.deep.equal([card1, card2, card3]);
   });
 
   it('should start round with no turns', function() {
-    const card1 = new Card();
-    const card2 = new Card();
-    const card3 = new Card();
-    const deck = new Deck([card1, card2, card3]);
-    const round = new Round(deck);
-
     expect(round.turns).to.equal(0);
   });
 
   it('should start round with no correct guesses', function() {
-    const card1 = new Card();
-    const card2 = new Card();
-    const card3 = new Card();
-    const deck = new Deck([card1, card2, card3]);
-    const round = new Round(deck);
-
     expect(round.correctGuesses).to.equal(0);
   });
 
   it('should start round with no incorrect guesses', function() {
-    const card1 = new Card();
-    const card2 = new Card();
-    const card3 = new Card();
-    const deck = new Deck([card1, card2, card3]);
-    const round = new Round(deck);
-
     expect(round.incorrectGuesses).to.deep.equal([]);
   });
 
-  it('should count turns', function() {
-    const card1 = new Card();
-    const card2 = new Card();
-    const card3 = new Card();
-    const deck = new Deck([card1, card2, card3]);
-    const round = new Round(deck);
-
-    round.takeTurn();
-
+  it.skip('should count turns', function() {
+    round.takeTurn(guess);
     expect(round.turns).to.equal(1);
   });
 
-  it.skip('should record a correct guess', function() {
-    const card1 = new Card();
-    const card2 = new Card();
-    const card3 = new Card();
-    const deck = new Deck([card1, card2, card3]);
-    const round = new Round(deck);
-
+  it('should instantiate a new Turn when a guess is made', function() {
     round.takeTurn();
+    expect(turn).to.be.an.instanceof(Turn);
+  });
+
+  it.skip('should record a correct guess', function() {
+    round.takeTurn(guess);
     expect(round.correctGuesses).to.equal(1);
   });
 
   it.skip('should record an incorrect guess', function() {
-    const card1 = new Card();
-    const card2 = new Card();
-    const card3 = new Card();
-    const deck = new Deck([card1, card2, card3]);
-    const round = new Round(deck);
-
-    round.takeTurn();
+    round.takeTurn(guess);
     expect(round.incorrectGuesses).to.deep.equal([card.id]);
   });
 
   it.skip('should return the current card being played', function() {
-    const card1 = new Card();
-    const card2 = new Card();
-    const card3 = new Card();
-    const deck = new Deck([card1, card2, card3]);
-    const round = new Round(deck);
-
     round.returnCurrentCard();
 
     expect(round.returnCurrentCard()).to.equal(card1.detail);
   });
 
-  it.skip('should instantiate a new Turn when a guess is made', function() {
-    const card1 = new Card();
-    const card2 = new Card();
-    const card3 = new Card();
-    const deck = new Deck([card1, card2, card3]);
-    const round = new Round(deck);
-
-    round.takeTurn();
-
-    expect(turn).to.be.an.instanceof(Turn);
-  });
-
   it.skip('should make the next card the current card', function() {
-    const card1 = new Card();
-    const card2 = new Card();
-    const card3 = new Card();
-    const deck = new Deck([card1, card2, card3]);
-    const round = new Round(deck);
-
-    round.takeTurn();
+    round.takeTurn(guess);
     round.returnCurrentCard();
 
     expect(round.returnCurrentCard()).to.equal(card2.detail);
   });
 
   it.skip('should calculate the percent correct', function() {
-    const card1 = new Card();
-    const card2 = new Card();
-    const card3 = new Card();
-    const deck = new Deck([card1, card2, card3]);
-    const round = new Round(deck);
-
     round.calculatePercentCorrect(5, 10);
-
     expect(round.calculatePercentCorrect()).to.equal(50);
   });
 
   it.skip('should end the round', function() {
-    const card1 = new Card();
-    const card2 = new Card();
-    const card3 = new Card();
-    const deck = new Deck([card1, card2, card3]);
-    const round = new Round(deck);
-
     round.endRound(50);
-
     expect(round.endRound).to.equal('** Round over! ** You answered 50% of the questions correctly!');
   });
 })
