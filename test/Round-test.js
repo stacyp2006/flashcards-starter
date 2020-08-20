3fe51a477444e8e984952f1d51473ba2364ae729
Stacy Potten <stacylynnaz@gmail.com>
2020-08-19 21:38:37 -0600

    Add test to verify Round class is a function

---
+++ test/Round-test.js
@@ -0,0 +1,18 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Card = require('../src/Card');
+const Deck = require('../src/deck');
+const Turn = require('../src/Turn');
+const Round = require('../src/Round');
+
+describe ('Round', function() {
+  it.skip('should be a function', function() {
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+
+    expect(Round).to.be.a('function');
+  });
+
+  
+})
